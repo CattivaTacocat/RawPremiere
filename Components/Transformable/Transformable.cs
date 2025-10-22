f539fb0a5fc5a953c6366431e0e1ad6e9f5a30e3
ImHuskyFrank <3460104296@qq.com>
2025-10-22 21:19:22 +0800

    变形组件v0.1

--- Components/Transformable/Transformable.cs
+++ Components/Transformable/Transformable.cs
@@ -5,9 +5,14 @@ namespace DeadDog.RawPremiere.Components;
 
 public partial class Transformable : Node
 {
-    #region 属性
+    #region 属性字段
     private Vector2 _position; 
     private Vector2 _scale;
     private float _rotation;
+    #endregion
+    #region 属性
+    [Export] public Vector2 Position { get; set; }
+    [Export] public Vector2 Scale { get; set; }
+    [Export] public float Rotation { get; set; }
     #endregion
 }
