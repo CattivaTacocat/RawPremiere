ceb8505012a5407a13450d295e3ec2dad9e79d41
occasionally-dodge <2247662110@qq.com>
2025-10-22 21:13:25 +0800

    变形控件

--- Components/Transformable/Transformable.cs
+++ Components/Transformable/Transformable.cs
@@ -6,6 +6,8 @@ namespace DeadDog.RawPremiere.Components;
 public partial class Transformable : Node
 {
     #region 属性
-    
+    private Vector2 _position; 
+    private Vector2 _scale;
+    private float _rotation;
     #endregion
 }
