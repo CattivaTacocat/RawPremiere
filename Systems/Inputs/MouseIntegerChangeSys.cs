f7169e5b3841e2d8f1eabb943924c4d0de6fdbd9
ImHuskyFrank <3460104296@qq.com>
2025-10-26 22:36:40 +0800

    鼠标改值系统初步

--- Systems/Inputs/MouseIntegerChangeSys.cs
+++ Systems/Inputs/MouseIntegerChangeSys.cs
@@ -20,12 +20,17 @@ public partial class MouseIntegerChangeSys : Node
     private Vector2 _startPos;
     private Vector2 _offset;
     private int _currentValue;
+
+    private bool _isRolling;
+    private float _rollerOffset;
     #endregion
     #region 操作
     //HACK:可能需要其他的辅助类进行封装
+    //FIXME:无法根据不同按键提供不同增量，必须要封装到一些辅助类了
     public override void _Input(InputEvent @event)
     {
         if (!HoverComp.IsHovered) return;
+        ShouldRollerToChange(@event);
         if (@event is not InputEventMouse em) return;
         switch (em)
         {
@@ -38,7 +43,7 @@ public partial class MouseIntegerChangeSys : Node
                 if (_isDragging)
                 {
                     _offset = emm.GlobalPosition - _startPos;
-                    ShouldWhatToChange(@event);
+                    ShouldMotionToChange(@event);
                 }
                 break;
             default:
@@ -75,28 +80,73 @@ public partial class MouseIntegerChangeSys : Node
     #endregion
     #region 决策
     //HACK:可能需要其他的辅助类进行封装
-    private void ShouldWhatToChange(InputEvent @event)
+    private void ShouldMotionToChange(InputEvent @event)
     {
+        if (MouseValueChangeComp.MouseChangeMethod == MouseChangeMethodEnum.Roller) return;
         float offset = 0;
         switch (MouseValueChangeComp.MouseChangeMethod)
         {
             case MouseChangeMethodEnum.Horizontal:
-                offset = _offset.X / 100;
+                    offset = _offset.X / 100;
                 break;
             case MouseChangeMethodEnum.Vertical:
-                offset = _offset.Y / -100;
-                break;
-            case MouseChangeMethodEnum.Roller:
-                if (@event is InputEventMouseButton emb)
-                    offset = emb.ButtonIndex switch
-                    {
-                        MouseButton.WheelUp => 1,
-                        MouseButton.WheelDown => -1,
-                        _ => offset
-                    };
+                    offset = _offset.Y / -100;
                 break;
         }
 
+        if (@event is not InputEventKey key)
+        {
+            NormalChangeValue(offset);
+            return;
+        }
+        switch (key.Keycode)
+        {
+            case Key.Ctrl:
+                CtrlChangeValue(offset);
+                break;
+            case Key.Shift:
+                ShiftChangeValue(offset);
+                break;
+            case Key.Alt:
+                AltChangeValue(offset);
+                break;
+            default:
+                NormalChangeValue(offset);
+                break;
+        }
+    }
+
+    //HACK:可能需要其他的辅助类进行封装
+    private void ShouldRollerToChange(InputEvent @event)
+    {
+        if (MouseValueChangeComp.MouseChangeMethod != MouseChangeMethodEnum.Roller) return;
+        if (@event is InputEventMouseButton em)
+        {
+            if (em.Pressed && em.ButtonIndex == MouseButton.Middle)
+            {
+                _isRolling = true;
+                _currentValue = WidgetComp.Value;
+                _rollerOffset = 0;
+            }
+            if (!em.Pressed && em.ButtonIndex == MouseButton.Middle)
+            {
+                _isRolling = false;
+            }
+            if (!_isRolling) return;
+            switch (em.ButtonIndex)
+            {
+                case MouseButton.WheelUp:
+                    _rollerOffset++;
+                    break;
+                case MouseButton.WheelDown:
+                    _rollerOffset--;
+                    break;
+            }
+        }
+
+        var offset = _rollerOffset;
+        
+        if (!_isRolling) return; 
         if (@event is not InputEventKey key)
         {
             NormalChangeValue(offset);
--- Temp/NCSTemp/NcsTest.cs
+++ Temp/NCSTemp/NcsTest.cs
@@ -4,8 +4,6 @@ public partial class NcsTest : Node
 {
     public override void _Ready()
     {
-        //TODO:准备做XXXX东西，什么什么东西准备完善
-        //FIXME:这个类有Bug，待修复
-        //HACK:这个方法是临时的，还需要更加完善的方案
+
     }
 }
